aebf4fc45f2bb79967c22777481be697a6602bca
Callum-Luxford <luxford.cal@gmail.com>
2024-01-12 10:28:36 +0000

    Update app.js

--- particle/app.js
+++ particle/app.js
@@ -18,7 +18,7 @@ particlesJS('particles-js',
       "number": {
         "value": 20,
         "density": {
-          "enable": false,
+          "enable": true,
           "value_area": 800
         }
       },
